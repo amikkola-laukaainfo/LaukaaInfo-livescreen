c42519f84b895cb31a7430825a184bd7c4694193
Antti <antti.mikkola@monitorilainen.fi>
2026-04-25 19:58:28 +0300

    refactor: remove cache-control and pragma no-cache headers from HTML head

--- dist/sw.js
+++ dist/sw.js
@@ -1,10 +1,10 @@
-const VERSION = '96e74a89'; // Päivitetty reittidatan latauksen korjausta varten
+const VERSION = '358ec3f3'; // Päivitetty reittidatan latauksen korjausta varten
 const CACHE_NAME = `laukaainfo-${VERSION}`;
 const ASSETS = [
     './',
     './index.html',
-    './style.f3fdb606.96e74a89.css',
-    './script.f3fdb606.96e74a89.js',
+    './style.f3fdb606.358ec3f3.css',
+    './script.f3fdb606.358ec3f3.js',
     './manifest.json',
     './icons/icon-192.png',
     './icons/icon-512.png',
