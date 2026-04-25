744d839b8e2af79ee809211ca2642455c8fbe1d3
Antti <antti.mikkola@monitorilainen.fi>
2026-04-25 19:42:32 +0300

    chore: rename screenshot build artifact to remove hash segment

--- dist/sw.js
+++ dist/sw.js
@@ -1,10 +1,10 @@
-const VERSION = 'f3fdb606'; // Päivitetty reittidatan latauksen korjausta varten
+const VERSION = '96e74a89'; // Päivitetty reittidatan latauksen korjausta varten
 const CACHE_NAME = `laukaainfo-${VERSION}`;
 const ASSETS = [
     './',
     './index.html',
-    './style.f3fdb606.css',
-    './script.f3fdb606.js',
+    './style.f3fdb606.96e74a89.css',
+    './script.f3fdb606.96e74a89.js',
     './manifest.json',
     './icons/icon-192.png',
     './icons/icon-512.png',
