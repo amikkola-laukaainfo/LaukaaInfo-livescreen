fe2745c991413c2b64cd730de7b7a8a054ba5ed3
Antti <antti.mikkola@monitorilainen.fi>
2026-05-06 20:53:28 +0300

    gfd

---
+++ scratch/check_varjola_pairings.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const profiling = JSON.parse(fs.readFileSync('d:/Projekteja\MUUTprojektit/LaukaaInfo-livescreen/LaukaaInfo-livescreen/company_profiling_data.json', 'utf8'));
+const varjolaId = 'company-263';
+const p = profiling.profiles[varjolaId];
+if (p && p.core && p.core.paired_with_by_context) {
+    console.log("Paired with by context:", JSON.stringify(p.core.paired_with_by_context, null, 2));
+} else {
+    console.log("NO PAIRED_WITH_BY_CONTEXT FOUND");
+}
