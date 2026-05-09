5ce9853b61e4298f648d65ef3a417cdcc0290c87
Antti <antti.mikkola@monitorilainen.fi>
2026-05-09 21:00:36 +0300

    feat: generate project distribution files and update profiling data structure

--- dist/sw.js
+++ dist/sw.js
@@ -1,10 +1,10 @@
-const VERSION = '69fd2cae'; // Päivitetty reittidatan latauksen korjausta varten
+const VERSION = 'f3fdb606'; // Päivitetty reittidatan latauksen korjausta varten
 const CACHE_NAME = `laukaainfo-${VERSION}`;
 const ASSETS = [
     './',
     './index.html',
-    './style.f3fdb606.69fd2cae.css',
-    './script.f3fdb606.69fd2cae.js',
+    './style.f3fdb606.css',
+    './script.f3fdb606.js',
     './manifest.json',
     './icons/icon-192.png',
     './icons/icon-512.png',
