c70b73aa0243f037e70f025778d7a5d091845b8d
Antti <antti.mikkola@monitorilainen.fi>
2026-05-09 21:26:43 +0300

    feat: generate build output and initialize profiling data for LaukaaInfo-livescreen

--- dist/sw.js
+++ dist/sw.js
@@ -1,10 +1,10 @@
-const VERSION = '3f4737db'; // Päivitetty reittidatan latauksen korjausta varten
+const VERSION = '6d4807d3'; // Päivitetty reittidatan latauksen korjausta varten
 const CACHE_NAME = `laukaainfo-${VERSION}`;
 const ASSETS = [
     './',
     './index.html',
-    './style.f3fdb606.3f4737db.css',
-    './script.f3fdb606.3f4737db.js',
+    './style.f3fdb606.6d4807d3.css',
+    './script.f3fdb606.6d4807d3.js',
     './manifest.json',
     './icons/icon-192.png',
     './icons/icon-512.png',
