9cb25f0e416caae6c2afd3b0ef7adbf2f7f942dc
Antti <antti.mikkola@monitorilainen.fi>
2026-05-11 22:48:49 +0300

    fds

--- dist/sw.js
+++ dist/sw.js
@@ -1,10 +1,10 @@
-const VERSION = 'f3fdb606'; // Päivitetty reittidatan latauksen korjausta varten
+const VERSION = '9570b587'; // Päivitetty reittidatan latauksen korjausta varten
 const CACHE_NAME = `laukaainfo-${VERSION}`;
 const ASSETS = [
     './',
     './index.html',
-    './style.f3fdb606.css',
-    './script.f3fdb606.js',
+    './style.f3fdb606.9570b587.css',
+    './script.f3fdb606.9570b587.js',
     './manifest.json',
     './icons/icon-192.png',
     './icons/icon-512.png',
