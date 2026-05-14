49d4600989e818a2d0f0c2df491624e857efbf67
Antti <antti.mikkola@monitorilainen.fi>
2026-05-14 14:06:31 +0300

    feat: initialize production distribution build with bundled assets and maintenance scripts

--- dist/sw.js
+++ dist/sw.js
@@ -1,10 +1,10 @@
-const VERSION = 'fca35c70'; // Päivitetty reittidatan latauksen korjausta varten
+const VERSION = 'd5490655'; // Päivitetty reittidatan latauksen korjausta varten
 const CACHE_NAME = `laukaainfo-${VERSION}`;
 const ASSETS = [
     './',
     './index.html',
-    './style.f3fdb606.fca35c70.css',
-    './script.f3fdb606.fca35c70.js',
+    './style.f3fdb606.d5490655.css',
+    './script.f3fdb606.d5490655.js',
     './manifest.json',
     './icons/icon-192.png',
     './icons/icon-512.png',
