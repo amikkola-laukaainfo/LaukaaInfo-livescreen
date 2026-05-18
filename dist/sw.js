202c8c174a202ac2da9f950c027cce32f41a21d9
Antti <antti.mikkola@monitorilainen.fi>
2026-05-18 23:21:34 +0300

    feat: rebuild application dist directory with updated profiling metadata and company data integration

--- dist/sw.js
+++ dist/sw.js
@@ -1,10 +1,10 @@
-const VERSION = 'e7d665e3'; // Päivitetty reittidatan latauksen korjausta varten
+const VERSION = '86a39bc6'; // Päivitetty reittidatan latauksen korjausta varten
 const CACHE_NAME = `laukaainfo-${VERSION}`;
 const ASSETS = [
     './',
     './index.html',
-    './style.f3fdb606.e7d665e3.css',
-    './script.f3fdb606.e7d665e3.js',
+    './style.f3fdb606.86a39bc6.css',
+    './script.f3fdb606.86a39bc6.js',
     './manifest.json',
     './icons/icon-192.png',
     './icons/icon-512.png',
