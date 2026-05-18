b299b6fc709b3f6ae2b28d7061c27230e462be5d
Antti <antti.mikkola@monitorilainen.fi>
2026-05-18 08:48:08 +0300

    feat: generate production distribution files and update profiling data and styles

--- dist/sw.js
+++ dist/sw.js
@@ -1,10 +1,10 @@
-const VERSION = 'b1db699d'; // Päivitetty reittidatan latauksen korjausta varten
+const VERSION = 'fd760f2b'; // Päivitetty reittidatan latauksen korjausta varten
 const CACHE_NAME = `laukaainfo-${VERSION}`;
 const ASSETS = [
     './',
     './index.html',
-    './style.f3fdb606.b1db699d.css',
-    './script.f3fdb606.b1db699d.js',
+    './style.f3fdb606.fd760f2b.css',
+    './script.f3fdb606.fd760f2b.js',
     './manifest.json',
     './icons/icon-192.png',
     './icons/icon-512.png',
