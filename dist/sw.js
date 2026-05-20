91d128ab997bfd9a0ea81094c3a42bd5e00ad886
Antti <antti.mikkola@monitorilainen.fi>
2026-05-20 23:16:11 +0300

    chore: build project and sync profiling metadata into distribution directory

--- dist/sw.js
+++ dist/sw.js
@@ -1,10 +1,10 @@
-const VERSION = 'c0081131'; // Päivitetty reittidatan latauksen korjausta varten
+const VERSION = 'd3a46712'; // Päivitetty reittidatan latauksen korjausta varten
 const CACHE_NAME = `laukaainfo-${VERSION}`;
 const ASSETS = [
     './',
     './index.html',
-    './style.f3fdb606.c0081131.css',
-    './script.f3fdb606.c0081131.js',
+    './style.f3fdb606.d3a46712.css',
+    './script.f3fdb606.d3a46712.js',
     './manifest.json',
     './icons/icon-192.png',
     './icons/icon-512.png',
