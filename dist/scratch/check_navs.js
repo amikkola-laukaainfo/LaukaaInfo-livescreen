36ae55060e222697d8bf6e8035c71601a1a71d08
Antti <antti.mikkola@monitorilainen.fi>
2026-06-12 19:39:42 +0300

    fgh

--- dist/scratch/check_navs.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const files = fs.readdirSync('.').filter(f => f.endsWith('.html'));
-files.forEach(f => {
-  const content = fs.readFileSync(f, 'utf8');
-  let missing = [];
-  if (!content.includes('class=\"desktop-nav\"')) missing.push('desktop-nav');
-  if (!content.includes('class=\"sidebar-menu\"') && !content.includes('id=\"sidebar-menu\"')) missing.push('sidebar-menu');
-  if (missing.length > 0) console.log(f + ' missing: ' + missing.join(', '));
-});
