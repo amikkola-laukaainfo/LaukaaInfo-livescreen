50945040609ca15306364cfd5e16a0d1cbcc129e
Antti <antti.mikkola@monitorilainen.fi>
2026-06-16 22:58:46 +0300

    gfd

--- dist/sw.js
+++ dist/sw.js
@@ -1,10 +1,10 @@
-const VERSION = 'c8647485'; // Päivitetty reittidatan latauksen korjausta varten
+const VERSION = '4a5dbe7d'; // Päivitetty reittidatan latauksen korjausta varten
 const CACHE_NAME = `laukaainfo-${VERSION}`;
 const ASSETS = [
     './',
     './index.html',
-    './style.f3fdb606.c8647485.css',
-    './script.f3fdb606.c8647485.js',
+    './style.f3fdb606.4a5dbe7d.css',
+    './script.f3fdb606.4a5dbe7d.js',
     './manifest.json',
     './icons/icon-192.png',
     './icons/icon-512.png',
