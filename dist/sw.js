0886f2e6ac96f41df49fb8f7c0c4b980e780526f
amikkola-laukaainfo <antti.mikkola@monitorilainen.fi>
2026-09-16 10:06:12 +0300

    gfd

--- dist/sw.js
+++ dist/sw.js
@@ -1,10 +1,10 @@
-const VERSION = 'dd2e1abe'; // Päivitetty reittidatan latauksen korjausta varten
+const VERSION = '2409cc79'; // Päivitetty reittidatan latauksen korjausta varten
 const CACHE_NAME = `laukaainfo-${VERSION}`;
 const ASSETS = [
     './',
     './index.html',
-    './style.f3fdb606.dd2e1abe.css',
-    './script.f3fdb606.dd2e1abe.js',
+    './style.f3fdb606.2409cc79.css',
+    './script.f3fdb606.2409cc79.js',
     './manifest.json',
     './icons/icon-192.png',
     './icons/icon-512.png',
