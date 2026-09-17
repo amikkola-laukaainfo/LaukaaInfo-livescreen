a31e27742deddbad63042745c4eb337996608aa8
amikkola-laukaainfo <antti.mikkola@monitorilainen.fi>
2026-09-17 14:24:12 +0300

    ghj

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const VERSION = '4aa2340b'; // Päivitetty reittidatan latauksen korjausta varten
+const VERSION = '05fca975'; // Päivitetty reittidatan latauksen korjausta varten
 const CACHE_NAME = `laukaainfo-${VERSION}`;
 const ASSETS = [
     './',
